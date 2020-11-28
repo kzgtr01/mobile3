103933fd2817f9962d9a6337856a1eecadfbfb03
kzgtr01 <65981924+kzgtr01@users.noreply.github.com>
2020-11-28 15:33:11 +0800

    main core upstream
    -new update to module
    -proper module leveling

---
+++ Assets/Scripts/BackButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButtonScript : MonoBehaviour {
+
+	// Load Main Menu scene when Back Button is pressed
+	public void BackToMainMenu()
+	{
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Load Scene 01 when start button is pressed
+	public void StartGame()
+	{
+		SceneManager.LoadScene ("Scene01");
+	}
+
+	// Load Trophy Room scene when trophy room button is pressed
+	public void GotoTrophyRoom()
+	{
+		SceneManager.LoadScene ("TrophyRoom");
+	}
+
+	// Reset All Player Preferences data to start earn trophies again
+	public void ResetPlayerPrefs()
+	{
+		PlayerPrefs.DeleteAll ();
+	}
+
+}
