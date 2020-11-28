83cd0cd1f8c2e972b908d785d7bf2caa22a4dfaf
kzgtr01 <65981924+kzgtr01@users.noreply.github.com>
2020-11-29 04:51:39 +0800

    upstream
    -core 1 completed

--- Assets/Scripts/LevelControlScript.cs
+++ Assets/Scripts/LevelControlScript.cs
@@ -11,7 +11,7 @@ public class LevelControlScript : MonoBehaviour {
 
 	// References to game objects that should be enabled
 	// when correct or incorrect answer is given
-	public GameObject correctSign, incorrectSign, cup, trophySing;
+	public GameObject correctSign, incorrectSign;
 
 	// Variable to contain current scene build index
 	int currentSceneIndex;
@@ -51,19 +51,9 @@ public class LevelControlScript : MonoBehaviour {
 		// Turn on "correct" sign
 		correctSign.gameObject.SetActive (true);
 
-		// Getting a value if you already got current trophy
-		int Cupgot = PlayerPrefs.GetInt(whichCupGot);
+		// then you proceed to next level
+		Invoke ("LoadNextLevel", 1f);
 
-		// If you already got current trophy
-		if (Cupgot == 1)
-
-			// then you proceed to next level
-			Invoke ("LoadNextLevel", 1f);
-
-		// if you don't have current trophy yet
-		else
-			// then GetTrophy method is invoked in 1 second
-			Invoke ("GetTrophy", 1f);
 	}
 
 	// Method is invoked if incorrect answer is given
@@ -82,25 +72,6 @@ public class LevelControlScript : MonoBehaviour {
 		Invoke ("GotoMainMenu", 1f);
 	}
 
-	// Method is invoked if you don't have current trophy yet
-	void GetTrophy()
-	{
-		// "Correct" sing is turned on
-		correctSign.SetActive (false);
-
-		// Trophy game object is shown
-		cup.SetActive (true);
-
-		// "You got new trophy" sign is shown
-		trophySing.SetActive (true);
-
-		// Set Player Prefs whichCupGot variable so you got this trophy
-		// to store this value between the scenes
-		PlayerPrefs.SetInt (whichCupGot, 1);
-
-		// Invoke LoadNextLevel method in 1 second
-		Invoke ("LoadNextLevel", 1f);
-	}
 
 	// Method loads next level depending on current scenes build index
 	void LoadNextLevel()
---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Nextlevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
