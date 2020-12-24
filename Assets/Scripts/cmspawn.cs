21b5911c71ebca478a29884059de2fa99be9f858
kzgtr01 <65981924+kzgtr01@users.noreply.github.com>
2020-12-25 05:37:43 +0800

    new module
    90%

---
+++ Assets/Scripts/cmspawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cmspawn : MonoBehaviour
+{
+    public GameObject pandacmPrefab;
+    public Transform[] spawnPoints;
+    public float gameTime;
+    public Text gameText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTime -= Time.deltaTime;
+        if(gameTime < 1)
+        {
+            gameTime = 0;
+         }
+         gameText.text = gameTime.ToString();
+    }   
+
+    public void Spawn()
+    {
+        GameObject pandacm = Instantiate(pandacmPrefab) as GameObject;
+        pandacm.transform.position = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+    }
+}
---
+++ Assets/Scripts/hammercontroller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hammercontroller : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+
+    private cmspawn ms;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        ms = GetComponent<cmspawn>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && ms.gameTime > 0)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if(hit.collider != null)
+            {
+                Destroy(hit.transform.gameObject);
+                score += 1;
+                scoreText.text = score.ToString();
+                ms.Spawn();
+            }
+        }
+    }
+}
